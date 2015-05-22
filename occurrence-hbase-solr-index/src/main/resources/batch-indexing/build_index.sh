52db785ec17bbec2d38a7c016908f64a5ee5a24a
Federico Mendez <fmendez@gbif.org>
2015-05-22 15:21:12 +0200

    parcel name changed from SOLR to CDH

--- occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
+++ occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
@@ -2,5 +2,5 @@
 ZK_HOST=$1
 #occurrence
 SOLR_COLLECTION=$2
-export HADOOP_CLASSPATH=lib/*:/opt/cloudera/parcels/SOLR/lib/hbase-solr/lib/*
-hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/SOLR/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars lib/ --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j conf/log4j.properties
+export HADOOP_CLASSPATH=lib/*:/opt/cloudera/parcels/CDH/lib/hbase-solr/lib/*
+hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars lib/ --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j conf/log4j.properties
