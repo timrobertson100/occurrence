ca1f6fdfc9c4931d84756f442a3e4e238bfaa730
Federico Mendez <fmendez@gbif.org>
2015-05-22 12:40:27 +0200

    creating occurrence-hbase-solr-index project, it contains bash scripts, morphlines and other resources to create Solr indeces from Hbase in a batch process or using the near-real-time indexing

---
+++ occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
@@ -0,0 +1,6 @@
+#c1n1.gbif.org:2181,c1n2.gbif.org:2181,c1n3.gbif.org:2181/solr
+ZK_HOST=$1
+#occurrence
+SOLR_COLLECTION=$2
+export HADOOP_CLASSPATH=lib/*:/opt/cloudera/parcels/SOLR/lib/hbase-solr/lib/*
+hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/SOLR/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars lib/ --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j conf/log4j.properties
