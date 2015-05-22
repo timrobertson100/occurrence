d6a4ad6961ed0a4579b7f1c0c902c504422df37d
Federico Mendez <fmendez@gbif.org>
2015-05-22 16:37:51 +0200

    adding zk host as parameter to the solrctl commands

--- occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
+++ occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
@@ -2,7 +2,7 @@ ZK_HOST=$1
 #occurrence
 SOLR_COLLECTION=$2
 #create solr configuration
-solrctl instancedir --create $SOLR_COLLECTION solr/
-solrctl collection --create $SOLR_COLLECTION -s 3 -c $SOLR_COLLECTION -r 2 -m 1 -n 3
+solrctl --zk $ZK_HOST instancedir --create $SOLR_COLLECTION solr/
+solrctl --zk $ZK_HOST collection --create $SOLR_COLLECTION -s 3 -c $SOLR_COLLECTION -r 2 -m 1 -n 3
 export HADOOP_CLASSPATH=lib/*:/opt/cloudera/parcels/CDH/lib/hbase-solr/lib/*
 hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars lib/ --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j conf/log4j.properties
