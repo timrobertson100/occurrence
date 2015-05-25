2c4dbdd8931064a0cf9ff031ce692cb51c6f8cd8
Federico Mendez <fmendez@gbif.org>
2015-05-25 12:22:19 +0200

    adding solr contrib libraries to the classpath

--- occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
+++ occurrence-hbase-solr-index/src/main/resources/batch-indexing/build_index.sh
@@ -11,5 +11,5 @@ set +e
 #create solr configuration
 solrctl --zk $ZK_HOST instancedir --create $SOLR_COLLECTION solr/
 solrctl --zk $ZK_HOST collection --create $SOLR_COLLECTION -s 3 -c $SOLR_COLLECTION -r 2 -m 2
-export HADOOP_CLASSPATH=lib/*:/opt/cloudera/parcels/CDH/lib/hbase-solr/lib/*
+export HADOOP_CLASSPATH=lib/*:/opt/cloudera/parcels/CDH/lib/hbase-solr/lib/*:/opt/cloudera/parcels/CDH/lib/solr/contrib/mr/*
 hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/CDH/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars lib/ --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j conf/log4j.properties
