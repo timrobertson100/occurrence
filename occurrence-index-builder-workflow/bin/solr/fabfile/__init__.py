cd03a30d1a7dc5f9b4bca8e4d14beb73cca0df2f
Federico Mendez <fmendez@gbif.org>
2015-10-08 13:35:36 +0200

    increasing cache sizes

--- occurrence-index-builder-workflow/bin/solr/fabfile/__init__.py
+++ occurrence-index-builder-workflow/bin/solr/fabfile/__init__.py
@@ -6,7 +6,7 @@ env.roledefs['cluster_dev'] = ['c1n1.gbif.org','c1n2.gbif.org','c1n3.gbif.org','
 
 env.roledefs['cluster_prod'] = ['c4n1.gbif.org','c4n2.gbif.org','c4n3.gbif.org','c4n4.gbif.org','c4n5.gbif.org','c4n6.gbif.org','c4n7.gbif.org','c4n8.gbif.org','c4n9.gbif.org','c4n10.gbif.org','c4n11.gbif.org','c4n12.gbif.org','prodmaster1-vh.gbif.org','prodmaster2-vh.gbif.org','prodmaster3-vh.gbif.org']
 
-env.roledefs['solr_uat'] = ['uatsolr1-vh.gbif.org','uatsolr2-vh.gbif.org','uatsolr3-vh.gbif.org']
+env.roledefs['solr_uat'] = ['uatsolr1-vh.gbif.org','uatsolr2-vh.gbif.org','uatsolr3-vh.gbif.org','uatsolr4-vh.gbif.org','uatsolr5-vh.gbif.org']
 
 
 @task
@@ -26,7 +26,7 @@ def install_solr_prod():
 def install_solr():
   run('''rm -rf /opt/solr-5.3.0/;\
         cd /tmp/;\
-        wget http://mirrors.ucr.ac.cr/apache//lucene/solr/5.3.0/solr-5.3.0.tgz;\
+        wget http://mirrors.dotsrc.org/apache/lucene/solr/5.3.0/solr-5.3.0.tgz;\
         tar xzf solr-5.3.0.tgz -C /opt/;\
         rm -f solr-5.3.0.tgz;''')
   copy_set_mr_path()
